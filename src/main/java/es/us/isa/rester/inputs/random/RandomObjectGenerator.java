0a4c1eada12c85d5db9dd44c1b828cbfed87d7a0
Alberto Martín López <alberto.martin95@gmail.com>
2019-01-11 13:23:36 +0100

    Added RandomObjectGenerator class (not implemented yet)

---
+++ src/main/java/es/us/isa/rester/inputs/random/RandomObjectGenerator.java
@@ -0,0 +1,18 @@
+package es.us.isa.rester.inputs.random;
+
+public class RandomObjectGenerator extends RandomGenerator {
+
+    public RandomObjectGenerator() {
+        super();
+    }
+
+    @Override
+    public Object nextValue() {
+        return null;
+    }
+
+    @Override
+    public String nextValueAsString() {
+        return null;
+    }
+}
