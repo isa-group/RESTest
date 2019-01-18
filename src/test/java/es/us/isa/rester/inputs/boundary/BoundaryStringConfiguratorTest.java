27711737f311337d69ef326ad30476c2c9f24c5a
Alberto Martín López <alberto.martin95@gmail.com>
2019-01-18 21:30:58 +0100

    Added tests for BoundaryStringConfigurator

---
+++ src/test/java/es/us/isa/rester/inputs/boundary/BoundaryStringConfiguratorTest.java
@@ -0,0 +1,89 @@
+package es.us.isa.rester.inputs.boundary;
+
+import static org.junit.Assert.*;
+
+import es.us.isa.rester.configuration.pojos.GenParameter;
+import es.us.isa.rester.configuration.pojos.Generator;
+import es.us.isa.rester.inputs.ITestDataGenerator;
+import es.us.isa.rester.inputs.TestDataGeneratorFactory;
+import es.us.isa.rester.inputs.fixed.InputValueIterator;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoundaryStringConfiguratorTest {
+
+    @Test
+    public void testConstructor() {
+        BoundaryStringConfigurator boundStrConf = new BoundaryStringConfigurator();
+        assertEquals("delta property should be 2 by default", 2, boundStrConf.getDelta());
+        assertEquals("maxLength property should be 1024 by default", 1024, boundStrConf.getMaxLength());
+        assertEquals("minLength property should be 0 by default", 0, boundStrConf.getMinLength());
+        assertEquals("includeEmptyString property should be true by default", true, boundStrConf.getIncludeEmptyString());
+        assertEquals("includeNullCharacter property should be true by default", true, boundStrConf.getIncludeNullCharacter());
+    }
+
+    @Test
+    public void testReturnedValuesSizeDefault() {
+        BoundaryStringConfigurator boundStrConf = new BoundaryStringConfigurator();
+        assertEquals("Wrong number of values returned", 14, boundStrConf.returnValues().size());
+    }
+
+    @Test
+    public void testReturnedValuesSizeNoSpecialCases() {
+        BoundaryStringConfigurator boundStrConf = new BoundaryStringConfigurator();
+        boundStrConf.setIncludeNullCharacter(false);
+        boundStrConf.setIncludeEmptyString(false);
+        assertEquals("Wrong number of values returned", 12, boundStrConf.returnValues().size());
+    }
+
+    @Test
+    public void testReturnedValuesSizeWithMinLengthMinusDeltaCases() {
+        BoundaryStringConfigurator boundStrConf = new BoundaryStringConfigurator();
+        boundStrConf.setMinLength(4);
+        assertEquals("Wrong number of values returned", 16, boundStrConf.returnValues().size());
+    }
+
+    @Test
+    public void testReturnValues() {
+        // Create BoundaryStringConfigurator
+        BoundaryStringConfigurator boundStrConf = new BoundaryStringConfigurator();
+
+        // Create generator and parameters
+        Generator gen = new Generator();
+        List<GenParameter> genParams = new ArrayList<>();
+
+        // Set minLength parameter so that  minLength-delta cases can be tested too
+        GenParameter minLengthParam = new GenParameter();
+        minLengthParam.setName("minLength");
+        List<String> paramValues = new ArrayList<>();
+        paramValues.add("4");
+        minLengthParam.setValues(paramValues);
+
+        genParams.add(minLengthParam);
+        gen.setGenParameters(genParams);
+        gen.setType("BoundaryString");
+
+        // Generate InputValueIterator of boundary strings
+        ITestDataGenerator abstractBoundStrIter = TestDataGeneratorFactory.createTestDataGenerator(gen);
+
+        assertEquals("Wrong next value obtained", "", abstractBoundStrIter.nextValue());
+        assertEquals("Wrong next value obtained", "\0", abstractBoundStrIter.nextValue());
+        assertEquals("Wrong string length for next value", 4, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 6, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1024, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1026, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 514, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 4, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 6, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1024, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1026, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 514, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 2, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 2, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1022, abstractBoundStrIter.nextValueAsString().length());
+        assertEquals("Wrong string length for next value", 1022, abstractBoundStrIter.nextValueAsString().length());
+    }
+
+}
