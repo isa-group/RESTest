652fe143b69a5836d003b89e1b271c867c4227fb
Alberto Martín López <alberto.martin95@gmail.com>
2020-01-29 13:26:33 +0100

    Added test for default test configuration generation of Events API

--- src/test/java/es/us/isa/restest/configuration/generators/DefaultTestConfigurationGeneratorTest.java
+++ src/test/java/es/us/isa/restest/configuration/generators/DefaultTestConfigurationGeneratorTest.java
@@ -233,4 +233,21 @@ public class DefaultTestConfigurationGeneratorTest {
 		DefaultTestConfigurationGenerator gen = new DefaultTestConfigurationGenerator(spec);
 		gen.generate(confPath, filters);
 	}
+
+	@Test
+	public void testEventsTestConfigurationGeneration() {
+
+		String specPath="src/test/resources/Events/swagger.yaml";
+		String confPath="src/test/resources/Events/testConf.yaml";
+		OpenAPISpecification spec = new OpenAPISpecification(specPath);
+
+		List<TestConfigurationFilter> filters = new ArrayList<TestConfigurationFilter>();
+		TestConfigurationFilter filter = new TestConfigurationFilter();
+		filter.setPath(null);		// null = All paths
+		filter.addAllMethods();
+		filters.add(filter);
+
+		DefaultTestConfigurationGenerator gen = new DefaultTestConfigurationGenerator(spec);
+		gen.generate(confPath, filters);
+	}
 }
