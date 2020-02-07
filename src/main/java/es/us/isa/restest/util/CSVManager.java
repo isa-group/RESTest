47e6c32d3e8b4e7943bb19fb3c5b420cc4865fae
Alberto Martín López <alberto.martin95@gmail.com>
2020-02-07 22:11:33 +0100

    Added some test resource files, added more functionality to CSVManager, including tests (close #14), and refactored some CSVManager tests

--- src/main/java/es/us/isa/restest/util/CSVManager.java
+++ src/main/java/es/us/isa/restest/util/CSVManager.java
@@ -36,6 +36,47 @@ public class CSVManager {
 	}
 
 	/**
+	 * Returns a list with the values of all rows (including header, if any)
+	 * of the input CSV file. Each row is a list of strings (one element per field)
+	 * @param path
+	 * @return
+	 */
+	public static List<List<String>> readCSV(String path) {
+		List<List<String>> rows = new ArrayList<>();
+
+		Reader in;
+		try {
+			in = new FileReader(path);
+			Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+			for (CSVRecord record : records) {
+				List<String> currentRow = new ArrayList<>();
+				for (String field: record)
+					currentRow.add(field);
+				rows.add(currentRow);
+			}
+		} catch (IOException ex) {
+			System.err.println("Error parsing CSV file: " + path + ". Message: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+
+		return rows;
+	}
+
+	/**
+	 * Returns a list with the values of all rows of the input CSV file. Each row
+	 * is a list of strings (one element per field)
+	 * @param path
+	 * @param includeFirstRow Whether to include first row of the CSV in the result
+	 *                        or not. Useful for excluding header.
+	 * @return
+	 */
+	public static List<List<String>> readCSV(String path, Boolean includeFirstRow) {
+		List<List<String>> rows = readCSV(path);
+		rows.remove(0);
+		return rows;
+	}
+
+	/**
 	 * Create a new CSV file in the given path and with the given header.
 	 * @param path Path where to place the file. Parent folders must be already created
 	 * @param header Header to add to the first line. If null, no header will be added
