de06c0e9b340fd3dfc3a7c1a8d45851e7ebd4d68
Alberto Martín López <alberto.martin95@gmail.com>
2020-02-16 21:08:01 +0100

    Updated ResponseValidationFilter
    This will hopefully fix #21

--- src/main/java/es/us/isa/restest/validation/ResponseValidationFilter.java
+++ src/main/java/es/us/isa/restest/validation/ResponseValidationFilter.java
@@ -7,17 +7,21 @@ import com.atlassian.oai.validator.report.ValidationReportFormatter;
 import com.atlassian.oai.validator.restassured.RestAssuredRequest;
 import com.atlassian.oai.validator.restassured.RestAssuredResponse;
 import com.atlassian.oai.validator.util.StringUtils;
+import com.atlassian.oai.validator.whitelist.ValidationErrorsWhitelist;
 import io.restassured.filter.FilterContext;
 import io.restassured.filter.OrderedFilter;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
 
+import static com.atlassian.oai.validator.whitelist.rule.WhitelistRules.*;
+
 /**
  * REST-Assured filter for validating ONLY responses from the API. The original
  * Atlassian OpenApiValidationFilter validates both the request and response. We
  * only want to validate the response, since we may intentionally send invalid inputs
- * to the API in order to create faulty test cases. Errors in the request will
+ * to the API in order to create faulty test cases. Request body validation errors
+ * (e.g. omitting a required property) will be whitelisted, while other errors will
  * be emitted as warnings.
  *
  * @author Alberto Martin-Lopez
@@ -32,6 +36,67 @@ public class ResponseValidationFilter implements OrderedFilter {
                         LevelResolver.create()
                                 .withLevel("validation.request", ValidationReport.Level.WARN)
                                 .build()
+                )
+                .withWhitelist(
+                        ValidationErrorsWhitelist.create()
+                                .withRule(
+                                        "Do not consider request schema validation errors as failures",
+                                        allOf(
+                                                isRequest(),
+                                                anyOf(
+                                                        // All validation keywords from JSON schema:
+                                                        // Any instance type:
+                                                        messageHasKey("validation.schema.const"),
+                                                        messageHasKey("validation.schema.type"),
+                                                        messageHasKey("validation.schema.enum"),
+                                                        messageHasKey("validation.schema.definitions"),
+                                                        // Numeric:
+                                                        messageHasKey("validation.schema.multipleOf"),
+                                                        messageHasKey("validation.schema.maximum"),
+                                                        messageHasKey("validation.schema.exclusiveMaximum"),
+                                                        messageHasKey("validation.schema.minimum"),
+                                                        messageHasKey("validation.schema.exclusiveMinimum"),
+                                                        // Strings:
+                                                        messageHasKey("validation.schema.maxLength"),
+                                                        messageHasKey("validation.schema.minLength"),
+                                                        messageHasKey("validation.schema.pattern"),
+                                                        messageHasKey("validation.schema.format"),
+                                                        messageHasKey("validation.schema.formatMaximum"),
+                                                        messageHasKey("validation.schema.formatMinimum"),
+                                                        messageHasKey("validation.schema.formatExclusiveMaximum"),
+                                                        messageHasKey("validation.schema.formatExclusiveMinimum"),
+                                                        // Arrays:
+                                                        messageHasKey("validation.schema.items"),
+                                                        messageHasKey("validation.schema.additionalItems"),
+                                                        messageHasKey("validation.schema.maxItems"),
+                                                        messageHasKey("validation.schema.minItems"),
+                                                        messageHasKey("validation.schema.contains"),
+                                                        messageHasKey("validation.schema.minContains"),
+                                                        messageHasKey("validation.schema.maxContains"),
+                                                        messageHasKey("validation.schema.uniqueItems"),
+                                                        // Objects:
+                                                        messageHasKey("validation.schema.required"),
+                                                        messageHasKey("validation.schema.dependentRequired"),
+                                                        messageHasKey("validation.schema.maxProperties"),
+                                                        messageHasKey("validation.schema.minProperties"),
+                                                        messageHasKey("validation.schema.additionalProperties"),
+                                                        messageHasKey("validation.schema.properties"),
+                                                        messageHasKey("validation.schema.patternProperties"),
+                                                        messageHasKey("validation.schema.dependencies"),
+                                                        messageHasKey("validation.schema.propertyNames"),
+                                                        messageHasKey("validation.schema.patternRequired"),
+                                                        // Compound:
+                                                        messageHasKey("validation.schema.allOf"),
+                                                        messageHasKey("validation.schema.anyOf"),
+                                                        messageHasKey("validation.schema.oneOf"),
+                                                        messageHasKey("validation.schema.not"),
+                                                        messageHasKey("validation.schema.if"),
+                                                        messageHasKey("validation.schema.then"),
+                                                        messageHasKey("validation.schema.else")
+
+                                                )
+                                        )
+                                )
                 )
                 .build();
     }
