fa8fa4766a12fba293d3dd36726d20a3487e187c
Alberto Martín López <alberto.martin95@gmail.com>
2020-02-19 12:51:19 +0100

    Added test for Travel API (which contains one dep) test case generation

---
+++ src/test/java/es/us/isa/restest/generators/TravelRandomTestCaseGenerator.java
@@ -0,0 +1,47 @@
+package es.us.isa.restest.generators;
+
+import es.us.isa.restest.configuration.TestConfigurationIO;
+import es.us.isa.restest.configuration.pojos.TestConfigurationObject;
+import es.us.isa.restest.specification.OpenAPISpecification;
+import es.us.isa.restest.testcases.TestCase;
+import es.us.isa.restest.testcases.writers.RESTAssuredWriter;
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+public class TravelRandomTestCaseGenerator {
+
+    @Test
+    public void travelFullTestCaseGenerator() {
+
+        // Load specification
+        String OAISpecPath = "src/test/resources/Travel/swagger.yaml";
+        OpenAPISpecification spec = new OpenAPISpecification(OAISpecPath);
+
+        // Load configuration
+        TestConfigurationObject conf = TestConfigurationIO.loadConfiguration("src/test/resources/Travel/testConf.yaml");
+
+        // Set number of test cases to be generated on each path
+        int numTestCases = 20;
+
+        // Create generator and filter
+        AbstractTestCaseGenerator generator = new RandomTestCaseGenerator(spec, conf, numTestCases);
+        generator.setFaultyRatio(0.2f);
+
+        Collection<TestCase> testCases = generator.generate();
+
+        assertEquals("Incorrect number of test cases", 120, testCases.size());
+
+        // Write test cases
+        String basePath = spec.getSpecification().getSchemes().get(0).name() + "://" + spec.getSpecification().getHost() + spec.getSpecification().getBasePath();
+        RESTAssuredWriter writer = new RESTAssuredWriter(OAISpecPath, "src/generation/java/restassured", "TravelTest", "restassured", basePath);
+        writer.setOAIValidation(true);
+        writer.write(testCases);
+    }
+
+
+
+
+}
