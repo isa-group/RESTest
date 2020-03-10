0c3c65e654bbf90914177dd8b116c66f41e02a3a
Alberto Martín López <alberto.martin95@gmail.com>
2020-03-10 19:40:31 +0100

    Added and updated Stripe resources

--- src/test/java/es/us/isa/restest/configuration/generators/DefaultTestConfigurationGeneratorTest.java
+++ src/test/java/es/us/isa/restest/configuration/generators/DefaultTestConfigurationGeneratorTest.java
@@ -440,5 +440,22 @@ public class DefaultTestConfigurationGeneratorTest {
 		gen.generate(confPath, filters);
 	}
 
+	@Test
+	public void testStripeTestConfigurationGeneration() {
+
+		String specPath="src/test/resources/Stripe/swagger.yaml";
+		String confPath="src/test/resources/Stripe/testConf.yaml";
+		OpenAPISpecification spec = new OpenAPISpecification(specPath);
+
+		List<TestConfigurationFilter> filters = new ArrayList<TestConfigurationFilter>();
+		TestConfigurationFilter filter = new TestConfigurationFilter();
+		filter.setPath(null);		// null = All paths
+		filter.addAllMethods();
+		filters.add(filter);
+
+		DefaultTestConfigurationGenerator gen = new DefaultTestConfigurationGenerator(spec);
+		gen.generate(confPath, filters);
+	}
+
 
 }
