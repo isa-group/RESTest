a2fc6dd8eb6c0a5be864247ab2dd3c4ea220911c
José Antonio Parejo Maestre <japarejo@gmail.com>
2020-07-23 13:15:41 +0200

    Updated RestAssured dependency.
    Also a small modification in the BikeWiseExperiment to use an ObjectiveFunction that requires test case execution

--- src/main/java/es/us/isa/restest/searchbased/experiment/BikeWiseExperiment.java
+++ src/main/java/es/us/isa/restest/searchbased/experiment/BikeWiseExperiment.java
@@ -13,6 +13,7 @@ import es.us.isa.restest.searchbased.objectivefunction.Coverage;
 import es.us.isa.restest.searchbased.objectivefunction.InputCoverage;
 import es.us.isa.restest.searchbased.objectivefunction.RestfulAPITestingObjectiveFunction;
 import es.us.isa.restest.searchbased.objectivefunction.SuiteSize;
+import es.us.isa.restest.searchbased.objectivefunction.UniqueFailures;
 import es.us.isa.restest.specification.OpenAPISpecification;
 import java.io.IOException;
 import java.util.List;
@@ -28,23 +29,23 @@ import static es.us.isa.restest.util.FileManager.createDir;
  */
 public class BikeWiseExperiment {
 
-    private static int fixedTestSuiteSize = 2;												// Number of test cases per suite
+    private static int fixedTestSuiteSize = 2; // Number of test cases per suite
     private static int minTestSuiteSize = 1;
     private static int maxTestSuiteSize = 10;
-    private static int populationSize = 10;												// Population size for the evolutionary algorithm
-    private static int maxEvaluations=1000;
-    private static String OAISpecPath = "src/test/resources/Bikewise/swagger.yaml";		    // Path to OAS specification file
-    private static String confPath = "src/test/resources/Bikewise/fullConf.yaml";		    // Path to test configuration file
-    private static String experimentName = "bikewise";                                      // Experiment name
-    private static String targetDir = "src/generation/java/searchbased";	// Directory where tests will be generated.
+    private static int populationSize = 10; // Population size for the evolutionary algorithm
+    private static int maxEvaluations = 1000;
+    private static String OAISpecPath = "src/test/resources/Bikewise/swagger.yaml"; // Path to OAS specification file
+    private static String confPath = "src/test/resources/Bikewise/fullConf.yaml"; // Path to test configuration file
+    private static String experimentName = "bikewise"; // Experiment name
+    private static String targetDir = "src/generation/java/searchbased"; // Directory where tests will be generated.
     private static long seed = 1979;
-    
+
     public static void main(String[] args) {
 
         createDir(targetDir);
-        
-    	List<RestfulAPITestingObjectiveFunction> objectiveFunctions=Lists.newArrayList(
-    			new InputCoverage(),
+
+        List<RestfulAPITestingObjectiveFunction> objectiveFunctions = Lists.newArrayList(
+                new UniqueFailures(),
     			new SuiteSize()
     	);
     	
