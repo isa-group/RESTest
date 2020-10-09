2ddb67ca0bdd91bd0e875059d8160d0cb0180c79
Alberto Martín López <alberto.martin95@gmail.com>
2020-10-09 09:20:35 +0200

    Updated Travis config

---
+++ install_dependencies.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=lib/JSONmutator.jar -DgroupId=es.us.isa -DartifactId=json-mutator -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/IDL2MiniZincMapper.jar -DgroupId=es.us.isa -DartifactId=idl-2-minizinc-mapper -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/IDLreasoner.jar -DgroupId=es.us.isa -DartifactId=idl-reasoner -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
