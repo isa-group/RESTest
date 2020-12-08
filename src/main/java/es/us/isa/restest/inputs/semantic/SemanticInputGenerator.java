279217f0cc749e3128414980cdf4d76c92452fa8
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2020-12-08 13:29:18 +0100

    DB OAS updated

--- src/main/java/es/us/isa/restest/inputs/semantic/SemanticInputGenerator.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/SemanticInputGenerator.java
@@ -29,7 +29,7 @@ import static es.us.isa.restest.configuration.generators.DefaultTestConfiguratio
 public class SemanticInputGenerator {
 
     // Properties file with configuration settings
-    private static String propertiesFilePath = "src/test/resources/SemanticAPIs/CommercialAPIs/AmadeusHotel/amadeusHotel.properties";
+    private static String propertiesFilePath = "src/test/resources/SemanticAPIs/CommercialAPIs/Yelp/yelp_semantic.properties";
     private static OpenAPISpecification spec;
     private static String OAISpecPath;
     private static String confPath;
