700795d594fe62ea9fd6c5398f8d07af1fa2c379
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2020-12-12 14:54:26 +0100

    OMDb tested

--- src/main/java/es/us/isa/restest/inputs/semantic/SemanticInputGenerator.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/SemanticInputGenerator.java
@@ -29,7 +29,7 @@ import static es.us.isa.restest.configuration.generators.DefaultTestConfiguratio
 public class SemanticInputGenerator {
 
     // Properties file with configuration settings
-    private static String propertiesFilePath = "src/test/resources/SemanticAPIs/CommercialAPIs/Marvel/marvel_random.properties";
+    private static String propertiesFilePath = "-----";
     private static OpenAPISpecification spec;
     private static String OAISpecPath;
     private static String confPath;
