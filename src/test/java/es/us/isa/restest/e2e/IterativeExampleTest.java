009a5eaa2cddaafab7250a3379a58ecb150ab363
José Ramón Fernández de la Rosa <josferde5@alum.us.es>
2021-01-26 19:46:06 +0100

    ART tests fixed

--- src/test/java/es/us/isa/restest/e2e/IterativeExampleTest.java
+++ src/test/java/es/us/isa/restest/e2e/IterativeExampleTest.java
@@ -27,19 +27,4 @@ public class IterativeExampleTest {
         assertTrue(checkIfExists("log/bikewise.log"));
     }
 
-    @Test
-    public void testARTIterativeExample() throws RESTestException {
-        String[] args = {"src/test/resources/AnApiOfIceAndFire/iceandfire_art.properties"};
-        TestGenerationAndExecution.main(args);
-
-        assertTrue(checkIfExists("src/generation/java/anApiOfIceAndFire"));
-
-        assertTrue(checkIfExists("target/allure-results/anApiOfIceAndFire"));
-        assertTrue(checkIfExists("target/allure-reports/anApiOfIceAndFire"));
-        assertTrue(checkIfExists("target/test-data/anApiOfIceAndFire/time.csv"));
-        assertTrue(checkIfExists("target/coverage-data/anApiOfIceAndFire"));
-        assertTrue(checkIfExists("target/test-data/anApiOfIceAndFire"));
-        assertTrue(checkIfExists("log/anApiOfIceAndFire.log"));
-    }
-
 }
