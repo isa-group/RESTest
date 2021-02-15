95c392be63682ea6f340f56d373b182916ef9c68
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-15 14:48:49 +0100

    TestConf updated

--- src/main/java/es/us/isa/restest/main/CreateTestConf.java
+++ src/main/java/es/us/isa/restest/main/CreateTestConf.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class CreateTestConf {
 
     private static final Logger log = LogManager.getLogger(CreateTestConf.class);
-    private static String openApiSpecPath = "----";			// OAS file path
+    private static String openApiSpecPath = "---";			// OAS file path
     private static String confPath;																// Test configuration path
 
     
