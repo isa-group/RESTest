ad4ae8ffb8e32cd0ac960d736331bf1cd3ea2b98
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 23:02:26 +0100

    TrueWayGeocoding test file updated

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/TrueWayGeocoding.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/TrueWayGeocoding.java
@@ -6,54 +6,34 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import static es.us.isa.restest.inputs.semantic.testing.MainTesting.printResponse;
+
 public class TrueWayGeocoding {
 
     private static final String baseUrl = "https://trueway-geocoding.p.rapidapi.com";
 
+    // language
     public static void trueWayGeocoding_language(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUrl + "/Geocode?address=505 Howard St, San Francisco&language=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
-
-
+        printResponse(url);
     }
 
+    // location
     public static void trueWayGeocoding_location(String semanticInput, String apiKey, String host) throws IOException {
         String url = baseUrl + "/ReverseGeocode?location=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
-
-
+        printResponse(url);
     }
+
+    // address
+    public static void trueWayGeocoding_address(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUrl + "/Geocode?address=" + semanticInput;
+        printResponse(url);
+    }
+
+    // address
+    public static void trueWayGeocoding_country(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUrl + "/Geocode?address=505 Howard St, San Francisco&country=" + semanticInput;
+        printResponse(url);
+    }
+
 }
