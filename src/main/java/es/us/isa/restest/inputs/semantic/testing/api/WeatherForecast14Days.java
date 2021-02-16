ab1a0cac046b0318694f346e7ec77b9f245baf55
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 23:15:02 +0100

    Update WeatherForecast14Days.java

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/WeatherForecast14Days.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/WeatherForecast14Days.java
@@ -6,82 +6,43 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import static es.us.isa.restest.inputs.semantic.testing.MainTesting.printResponse;
+
 public class WeatherForecast14Days {
 
     private static final String baseUri = "https://weather-forecast-14-days.p.rapidapi.com";
 
     // GET /api/countrycitylist
     public static void weatherForecast14Days_apiCountryCityList_country(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/api/countrycitylist?COUNTRY=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
-
-
+        printResponse(url);
     }
 
-    // GET /api/countrycitylist
+    // GET /api/getforecastdata
+    // LON
     public static void weatherForecast14Days_getForecastData_LON(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/api/getforecastdata?LON=" + semanticInput + "&LAT=42.6306";
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
-
-
+        printResponse(url);
     }
 
+    // LAT
     public static void weatherForecast14Days_getForecastData_LAT(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/api/getforecastdata?LON=-74.6594&LAT=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
+    }
 
 
+    // GET /api/getlocation
+    // city
+    public static void weatherForecast14Days_getLocation_city(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUri + "/api/getlocation?city=" + semanticInput;
+        printResponse(url);
+    }
+
+    // zipcode
+    public static void weatherForecast14Days_getLocation_zipcode(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUri + "/api/getlocation?ZIPCODE=" + semanticInput;
+        printResponse(url);
     }
 
 }
