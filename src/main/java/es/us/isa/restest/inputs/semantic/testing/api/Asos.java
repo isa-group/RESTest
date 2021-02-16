c6f8f59f513b7bf9d2bd388277fe16a295101ced
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 20:24:28 +0100

    Asos test file updated

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/Asos.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/Asos.java
@@ -11,7 +11,7 @@ public class Asos {
     private static final String baseUri = "https://asos2.p.rapidapi.com";
 
 //    GET /v2/auto-complete
-
+    // country
     public static void asos_v2AutoComplete_country(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/v2/auto-complete" + "?q=bikini&country=" + semanticInput;
@@ -36,6 +36,7 @@ public class Asos {
 
     }
 
+    // store
     public static void asos_v2AutoComplete_store(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/v2/auto-complete" + "?q=bikini&store=" + semanticInput;
@@ -60,8 +61,54 @@ public class Asos {
 
     }
 
-//    /products/v3/list-similarities
+    // currency
+    public static void asos_v2AutoComplete_currency(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/v2/auto-complete" + "?q=bikini&currency=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+    // lang
+    public static void asos_v2AutoComplete_lang(String semanticInput, String apiKey, String host) throws IOException {
+
+        String url = baseUri + "/v2/auto-complete" + "?q=bikini&lang=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+//    /products/v3/list-similarities
+    // currency
     public static void asos_productv3ListSimilarities_currency(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/products/v3/list-similarities" + "?id=9851612&currency=" + semanticInput;
@@ -82,12 +129,57 @@ public class Asos {
         System.out.println("RESPONSE CODE: " + response.code());
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
-
-
     }
 
-//    GET /products/v3/detail
+    // store
+    public static void asos_productv3ListSimilarities_store(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/products/v3/list-similarities" + "?id=9851612&store=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+    // lang
+    public static void asos_productv3ListSimilarities_lang(String semanticInput, String apiKey, String host) throws IOException {
+
+        String url = baseUri + "/products/v3/list-similarities" + "?id=9851612&lang=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+
+//    GET /products/v3/detail
+    // lang
     public static void asos_productv3Detail_lang(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/products/v3/detail" + "?id=9851612&lang=" + semanticInput;
@@ -112,6 +204,7 @@ public class Asos {
 
     }
 
+    // currency
     public static void asos_productv3Detail_currency(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/products/v3/detail" + "?id=9851612&currency=" + semanticInput;
@@ -132,11 +225,33 @@ public class Asos {
         System.out.println("RESPONSE CODE: " + response.code());
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
+    }
 
+    // store
+    public static void asos_productv3Detail_store(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/products/v3/detail" + "?id=9851612&store=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
     }
 
     // GET /categories/list
+    // country
     public static void asos_categoriesList_country(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/categories/list?country=" + semanticInput;
@@ -158,8 +273,32 @@ public class Asos {
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
 
+    }
+
+    // lang
+    public static void asos_categoriesList_lang(String semanticInput, String apiKey, String host) throws IOException {
+
+        String url = baseUri + "/categories/list?lang=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
 
     }
+
 
 
 
