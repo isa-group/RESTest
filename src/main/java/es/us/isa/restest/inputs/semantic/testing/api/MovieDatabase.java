1729bf66394d1d4be142a74ed406f2867abdbd67
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 21:36:32 +0100

    Movie database test file updated

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/MovieDatabase.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/MovieDatabase.java
@@ -6,53 +6,21 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import static es.us.isa.restest.inputs.semantic.testing.MainTesting.printResponse;
+
 public class MovieDatabase {
 
     private static final String baseUri = "https://movie-database-imdb-alternative.p.rapidapi.com";
 
     public static void movieDatabase_y(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/?i=tt0201567&y=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
 
 
     public static void movieDatabase_s(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/?s=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
 
 }
