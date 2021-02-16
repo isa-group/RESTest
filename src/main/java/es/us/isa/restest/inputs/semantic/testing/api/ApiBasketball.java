97be4696dbed1f19173996325290840046a03e64
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 19:55:59 +0100

    ApiBasketball test file updated

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/ApiBasketball.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/ApiBasketball.java
@@ -14,6 +14,7 @@ public class ApiBasketball {
     private static final String baseUri = "https://api-basketball.p.rapidapi.com";
 
     // GET /standings
+    // season
     public static String apiBasketball_standings_season(String semanticInput, String apiKey, String host) throws IOException, JSONException {
 
         String url = baseUri + "/standings?league=12&season=" + semanticInput;
@@ -32,13 +33,57 @@ public class ApiBasketball {
         Response response = client.newCall(request).execute();
 
         System.out.println("RESPONSE CODE: " + response.code());
-//        System.out.println(response.body().string());
-
-//        JSONObject Jobject = new JSONObject(response.body().string());
-//        JSONArray Jarray = Jobject.getJSONArray("errors");
 //        System.out.println("--------------------------------------------------------------------------------------");
 
         return response.body().string();
+    }
+
+    // stage
+    public static void apiBasketball_standings_stage(String semanticInput, String apiKey, String host) throws IOException, JSONException {
+
+        String url = baseUri + "/standings?league=12&season=2019&stage=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+
+    }
+
+    // group
+    public static void apiBasketball_standings_group(String semanticInput, String apiKey, String host) throws IOException, JSONException {
+
+        String url = baseUri + "/standings?league=12&season=2019&group=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+
     }
 
     // GET /standings/groups
@@ -62,11 +107,33 @@ public class ApiBasketball {
         System.out.println("RESPONSE CODE: " + response.code());
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
+    }
 
+    // GET /standing/stages
+    public static void apiBasketball_standingsStages_season(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/standings/stages?league=12&season=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
     }
 
     // GET /leagues
+    // season
     public static void apiBasketball_leagues_season(String semanticInput, String apiKey, String host) throws IOException {
 
         String url = baseUri + "/leagues?season=" + semanticInput;
@@ -87,8 +154,75 @@ public class ApiBasketball {
         System.out.println("RESPONSE CODE: " + response.code());
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
+    }
 
+    // name
+    public static void apiBasketball_leagues_name(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/leagues?name=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+    // country
+    public static void apiBasketball_leagues_country(String semanticInput, String apiKey, String host) throws IOException {
+
+        String url = baseUri + "/leagues?country=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
+    }
+
+    // country
+    public static void apiBasketball_leagues_code(String semanticInput, String apiKey, String host) throws IOException {
+
+        String url = baseUri + "/leagues?code=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
     }
 
     // GET /countries
@@ -112,8 +246,28 @@ public class ApiBasketball {
         System.out.println("RESPONSE CODE: " + response.code());
         System.out.println(response.body().string());
         System.out.println("--------------------------------------------------------------------------------------");
+    }
 
+    public static void apiBasketball_countries_name(String semanticInput, String apiKey, String host) throws IOException {
 
+        String url = baseUri + "/countries?name=" + semanticInput;
+
+        System.out.println(url);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("x-rapidapi-host", host)
+                .addHeader("x-rapidapi-key", apiKey)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println("RESPONSE CODE: " + response.code());
+        System.out.println(response.body().string());
+        System.out.println("--------------------------------------------------------------------------------------");
     }
 
 
