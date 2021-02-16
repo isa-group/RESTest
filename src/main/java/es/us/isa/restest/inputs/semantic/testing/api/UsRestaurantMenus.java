5648c89a8e8a2da68f985663f4550979782417db
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 23:04:22 +0100

    Update UsRestaurantMenus.java

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/UsRestaurantMenus.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/UsRestaurantMenus.java
@@ -6,6 +6,8 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import static es.us.isa.restest.inputs.semantic.testing.MainTesting.printResponse;
+
 public class UsRestaurantMenus {
 
     private static final String baseUri = "https://us-restaurant-menus.p.rapidapi.com";
@@ -13,120 +15,32 @@ public class UsRestaurantMenus {
 
     // Test latitude (The operation name must be specified)
     public static void usRestaurantMenus_latitude(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + operationPath + "?lon=-73.992378&lat=" + semanticInput + "&distance=10000";
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
 
     // Test longitude (The operation name must be specified)
     public static void usRestaurantMenus_longitude(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + operationPath + "?lon=" + semanticInput + "&lat=40.68919&distance=10000";
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
 
     // Test distance (The operation name must be specified)
     public static void usRestaurantMenus_distance(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + operationPath + "?lon=-73.992378&lat=40.68919&distance=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
-
-
+    
     // GET /restaurants/zip_code/{zip_code}
     public static void usRestaurantMenus_restaurantsZipcode_zipCode(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/restaurants/zip_code/" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
-
-
 
 //    GET /restaurants/state/{state}
     public static void usRestaurantMenus_restaurantsState_state(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUri + "/restaurants/state/" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
+        printResponse(url);
     }
 
 }
