67f1196e070bb282923a41bf1de4c6b281245959
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-02-16 21:41:40 +0100

    OpenWeatherMap test file updated

--- src/main/java/es/us/isa/restest/inputs/semantic/testing/api/OpenWeatherMap.java
+++ src/main/java/es/us/isa/restest/inputs/semantic/testing/api/OpenWeatherMap.java
@@ -6,33 +6,30 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import static es.us.isa.restest.inputs.semantic.testing.MainTesting.printResponse;
+
 public class OpenWeatherMap {
 
     private static final String baseUrl = "https://community-open-weather-map.p.rapidapi.com";
+    private static final String operationPath = "/forecast";
 
     public static void openWeatherMap_forecast_zip(String semanticInput, String apiKey, String host) throws IOException {
-
         String url = baseUrl + "/forecast?zip=" + semanticInput;
-
-        System.out.println(url);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-host", host)
-                .addHeader("x-rapidapi-key", apiKey)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("RESPONSE CODE: " + response.code());
-        System.out.println(response.body().string());
-        System.out.println("--------------------------------------------------------------------------------------");
-
-
+        printResponse(url);
     }
 
+    // Latitude
+    // https://community-open-weather-map.p.rapidapi.com/find?lat=42.6461&lon=-1.50802
+    public static void openWeatherMap_latitude(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUrl + operationPath + "?lat=" + semanticInput + "&lon=-1.50802";
+        printResponse(url);
+    }
 
+    // Longitude
+    // https://community-open-weather-map.p.rapidapi.com/find?lat=40.3513&lon=-74.4801
+    public static void openWeatherMap_longitude(String semanticInput, String apiKey, String host) throws IOException {
+        String url = baseUrl + operationPath + "??lat=40.3513&lon=" + semanticInput;
+        printResponse(url);
+    }
+    
 }
