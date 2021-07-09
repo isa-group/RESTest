829e7f752a381d7b9c0f6a35ba989f8c79ec9ba7
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-07-09 18:08:14 +0200

    Updated

--- src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
+++ src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
@@ -35,7 +35,7 @@ import static es.us.isa.restest.util.Timer.TestStep.ALL;
 public class TestGenerationAndExecution {
 
 	// Properties file with configuration settings
-	private static String propertiesFilePath = "src/test/resources/SemanticAPIs/Yelp/yelp_semantic.properties";
+	private static String propertiesFilePath = "---";
 	
 	private static Integer numTestCases; 								// Number of test cases per operation
 	private static String OAISpecPath; 									// Path to OAS specification file
