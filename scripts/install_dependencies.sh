1bd405d7713b83ce76bcabd776abde16da64708e
Alberto Martín López <alberto.martin95@gmail.com>
2021-07-28 16:34:22 -0700

    CircleCI

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,2 @@
-mvn install:install-file -Dfile=lib/idl-choco.jar -DgroupId=es.us.isa -DartifactId=idl-choco -Dversion=1.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/idlreasoner-choco-1.0.0.jar -DgroupId=es.us.isa -DartifactId=idlreasoner-choco -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/idl-choco-1.0.0.jar -DgroupId=es.us.isa -DartifactId=idl-choco -Dversion=1.0.0 -Dpackaging=jar
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=lib/idlreasoner-choco-1.0.0.jar
