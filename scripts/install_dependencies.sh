ba349a8cdf58c31e9c299857a686f8ef309fbaa8
Alberto Martín López <alberto.martin95@gmail.com>
2021-07-28 13:10:01 -0700

    Replaced IDLReasoner with IDLReasonerChoco

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=lib/idl-choco.jar -DgroupId=es.us.isa -DartifactId=idl-choco -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/idlreasoner-choco-1.0.0.jar -DgroupId=es.us.isa -DartifactId=idlreasoner-choco -Dversion=1.0.0 -Dpackaging=jar
