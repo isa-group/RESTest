7b5d993efc42c94ca695ca97ee4572912052d2c1
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-07-30 21:26:36 +0200

    jar dependencies updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,4 @@
 mvn install:install-file -Dfile=lib/idl-choco-1.0.0.jar -DgroupId=es.us.isa -DartifactId=idl-choco -Dversion=1.0.0 -Dpackaging=jar
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=lib/idlreasoner-choco-1.0.0.jar
+mvn install:install-file -Dfile=lib/MaleRegexTree.jar -DgroupId=es.us.isa -DartifactId=MaleRegexTree -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/RandomRegexTurtle.jar -DgroupId=es.us.isa -DartifactId=RandomRegexTurtle -Dversion=1.0.0 -Dpackaging=jar
