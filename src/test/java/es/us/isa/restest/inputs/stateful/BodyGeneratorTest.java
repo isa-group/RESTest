92117b687217ba7894f0c856c803bad6f020ce29
Alberto Martín López <alberto.martin95@gmail.com>
2021-09-01 15:30:23 -0700

    Stateful data is now type-aware (i.e., more than strings)
    (cherry picked from commit 43b77100f22ea6ec192b50d32873fd6fb170e203)

--- src/test/java/es/us/isa/restest/inputs/stateful/BodyGeneratorTest.java
+++ src/test/java/es/us/isa/restest/inputs/stateful/BodyGeneratorTest.java
@@ -117,4 +117,57 @@ public class BodyGeneratorTest {
             fail("The body could not be parsed as a JSON");
         }
     }
+
+    @Test
+    public void bodyGenerationDiverseDataTest() {
+        OpenAPISpecification spec = new OpenAPISpecification("src/test/resources/Comments/swagger_forTestSuite5.yaml");
+        String operationPath = "/diverse/body/data";
+        Operation oasOperation = spec.getSpecification().getPaths().get(operationPath).getPost();
+        String dataDirPath = "src/test/resources/jsonData/stateful_body1";
+
+        GenParameter defaultValue = new GenParameter();
+        defaultValue.setName("defaultValue");
+        defaultValue.setValues(Collections.singletonList("{}"));
+
+        generator.getGenParameters().add(defaultValue);
+
+        BodyGenerator gen = (BodyGenerator) TestDataGeneratorFactory.createTestDataGenerator(generator);
+        gen.setSpec(spec);
+        gen.setDataDirPath(dataDirPath);
+        gen.setOpenApiOperation(oasOperation);
+        gen.setOperation("GET", operationPath);
+
+        String value = gen.nextValueAsString(false);
+
+        assertNotNull("The generator could not generate a body parameter", value);
+
+        try {
+            JsonNode body = (new ObjectMapper()).readTree(value);
+
+            // Assert all fields
+            assertTrue(body.get("numberParam").isNumber());
+            assertTrue(body.get("numberParam").intValue() == 1 || body.get("numberParam").intValue() == 2);
+            assertTrue(body.get("numberParam2").isTextual());
+            assertEquals("stringExample", body.get("numberParam2").textValue());
+            assertTrue(body.get("booleanParam").isBoolean());
+            assertTrue(body.get("booleanParam").booleanValue() == true || body.get("booleanParam").booleanValue() == false);
+            assertTrue(body.get("booleanParam2").isNull());
+            assertEquals(null, body.get("booleanParam2").textValue());
+            assertTrue(body.get("enumParam").isTextual());
+            assertTrue(body.get("enumParam").textValue().equals("Review") || body.get("enumParam").textValue().equals("Request"));
+            if (body.get("stringParam").isTextual())
+                assertTrue(body.get("stringParam").textValue().equals("Example 1") || body.get("stringParam").textValue().equals("Example 2"));
+            else if (body.get("stringParam").isNumber())
+                assertEquals(1, body.get("stringParam").intValue());
+            else if (body.get("stringParam").isBoolean())
+                assertEquals(false, body.get("stringParam").booleanValue());
+            else if (body.get("stringParam").isNull())
+                assertEquals(null, body.get("stringParam").textValue());
+            else
+                fail("The property 'stringParam' should be either string, number, boolean or null");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            fail("The body could not be parsed as a JSON");
+        }
+    }
 }
--- src/test/java/es/us/isa/restest/testcases/restassured/filters/StatefulFilterTest.java
+++ src/test/java/es/us/isa/restest/testcases/restassured/filters/StatefulFilterTest.java
@@ -1,11 +1,17 @@
 package es.us.isa.restest.testcases.restassured.filters;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.fasterxml.jackson.databind.node.NumericNode;
 import es.us.isa.restest.util.FileManager;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static es.us.isa.restest.util.FileManager.readFile;
 import static org.junit.Assert.*;
@@ -14,14 +20,16 @@ public class StatefulFilterTest {
 
     @BeforeClass
     public static void setUp() {
-        RestAssured.baseURI = "http://anapioficeandfire.com/";
+        RestAssured.baseURI = "https://restest-tests-server.herokuapp.com";
     }
 
     @Test
-    public void shouldSaveResponsesInJson() {
+    public void shouldSaveResponsesInJsonAndStoreCorrectValues() {
         String dirPath = "src/test/resources/restest-test-resources";
         StatefulFilter filter = new StatefulFilter(dirPath);
-        filter.setOperation("GET", "/api/characters/{character-id}");
+        filter.setOperation("GET", "/stateful/filter/{id}");
+
+        FileManager.deleteFile(dirPath + "/stateful_data.json");
 
         try {
             Response response = RestAssured
@@ -29,7 +37,8 @@ public class StatefulFilterTest {
                     .log().all()
                     .filter(filter)
                     .when()
-                    .get("/api/characters/823");
+                    .pathParam("id", "1")
+                    .get("/stateful/filter/{id}");
 
             response.then().log().all();
         } catch (RuntimeException ex) {
@@ -38,6 +47,37 @@ public class StatefulFilterTest {
         }
 
         assertTrue("The JSON file was not created", FileManager.checkIfExists(dirPath + "/stateful_data.json"));
+        try {
+            JsonNode dict = new ObjectMapper().readTree(new File(dirPath + "/stateful_data.json"));
+            JsonNode operationDict = dict.get("GET/stateful/filter/{id}");
+
+            // Assert all elements saved in stateful_data.json
+            assertNotNull(operationDict.get("object1.numberProp1"));
+            assertEquals(4, operationDict.get("object1.numberProp1").size());
+            assertTrue(operationDict.get("object1.numberProp1").get(0).isNumber());
+            assertEquals(1, operationDict.get("object1.numberProp1").get(0).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(1).isNumber());
+            assertEquals(2, operationDict.get("object1.numberProp1").get(1).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(2).isNumber());
+            assertEquals(3, operationDict.get("object1.numberProp1").get(2).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(3).isTextual());
+            assertEquals("3", operationDict.get("object1.numberProp1").get(3).textValue());
+            assertNotNull(operationDict.get("object1.numberProp2"));
+            assertEquals(1, operationDict.get("object1.numberProp2").size());
+            assertTrue(operationDict.get("object1.numberProp2").get(0).isNull());
+            assertEquals(null, operationDict.get("object1.numberProp2").get(0).textValue());
+            assertNotNull(operationDict.get("stringProp1"));
+            assertEquals(1, operationDict.get("stringProp1").size());
+            assertTrue(operationDict.get("stringProp1").get(0).isTextual());
+            assertEquals("string1", operationDict.get("stringProp1").get(0).textValue());
+            assertNotNull(operationDict.get("booleanProp"));
+            assertEquals(1, operationDict.get("booleanProp").size());
+            assertTrue(operationDict.get("booleanProp").get(0).isBoolean());
+            assertEquals(false, operationDict.get("booleanProp").get(0).booleanValue());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            fail(ex.getMessage());
+        }
 
         FileManager.deleteFile(dirPath + "/stateful_data.json");
     }
@@ -46,7 +86,9 @@ public class StatefulFilterTest {
     public void shouldNotRepeatValuesInJson() {
         String dirPath = "src/test/resources/restest-test-resources";
         StatefulFilter filter = new StatefulFilter(dirPath);
-        filter.setOperation("GET", "/api/characters/{character-id}");
+        filter.setOperation("GET", "/stateful/filter/{id}");
+
+        FileManager.deleteFile(dirPath + "/stateful_data.json");
 
         try {
             Response response = RestAssured
@@ -54,7 +96,8 @@ public class StatefulFilterTest {
                     .log().all()
                     .filter(filter)
                     .when()
-                    .get("/api/characters/823");
+                    .pathParam("id", "1")
+                    .get("/stateful/filter/{id}");
 
             response.then().log().all();
         } catch (RuntimeException ex) {
@@ -63,7 +106,7 @@ public class StatefulFilterTest {
         }
 
         assertTrue("The JSON file was not created", FileManager.checkIfExists(dirPath + "/stateful_data.json"));
-        String jsonContent = readFile(dirPath + "/stateful_data.json");
+        String firstDict = readFile(dirPath + "/stateful_data.json");
 
         try {
             Response response = RestAssured
@@ -71,7 +114,8 @@ public class StatefulFilterTest {
                     .log().all()
                     .filter(filter)
                     .when()
-                    .get("/api/characters/823");
+                    .pathParam("id", "1")
+                    .get("/stateful/filter/{id}");
 
             response.then().log().all();
         } catch (RuntimeException ex) {
@@ -79,7 +123,112 @@ public class StatefulFilterTest {
             fail(ex.getMessage());
         }
 
-        assertEquals("The JSON file should not have been updated", jsonContent, readFile(dirPath + "/stateful_data.json"));
+        String secondDict = readFile(dirPath + "/stateful_data.json");
+
+        assertEquals("The JSON file should not have been updated", firstDict, secondDict);
+
+        FileManager.deleteFile(dirPath + "/stateful_data.json");
+    }
+
+    @Test
+    public void shouldNotRepeatValuesInJsonAndUpdate() {
+        String dirPath = "src/test/resources/restest-test-resources";
+        StatefulFilter filter = new StatefulFilter(dirPath);
+        filter.setOperation("GET", "/stateful/filter/{id}");
+
+        FileManager.deleteFile(dirPath + "/stateful_data.json");
+
+        try {
+            Response response = RestAssured
+                    .given()
+                    .log().all()
+                    .filter(filter)
+                    .when()
+                    .pathParam("id", "1")
+                    .get("/stateful/filter/{id}");
+
+            response.then().log().all();
+        } catch (RuntimeException ex) {
+            System.err.println(ex.getMessage());
+            fail(ex.getMessage());
+        }
+
+        assertTrue("The JSON file was not created", FileManager.checkIfExists(dirPath + "/stateful_data.json"));
+        String firstDict = readFile(dirPath + "/stateful_data.json");
+
+        try {
+            Response response = RestAssured
+                    .given()
+                    .log().all()
+                    .filter(filter)
+                    .when()
+                    .pathParam("id", "1")
+                    .get("/stateful/filter/{id}");
+
+            response.then().log().all();
+        } catch (RuntimeException ex) {
+            System.err.println(ex.getMessage());
+            fail(ex.getMessage());
+        }
+
+        String secondDict = readFile(dirPath + "/stateful_data.json");
+
+        assertEquals("The JSON file should not have been updated", firstDict, secondDict);
+
+        try {
+            Response response = RestAssured
+                    .given()
+                    .log().all()
+                    .filter(filter)
+                    .when()
+                    .pathParam("id", "2")
+                    .get("/stateful/filter/{id}");
+
+            response.then().log().all();
+        } catch (RuntimeException ex) {
+            System.err.println(ex.getMessage());
+            fail(ex.getMessage());
+        }
+
+        String thirdDict = readFile(dirPath + "/stateful_data.json");
+
+        try {
+            JsonNode dict = new ObjectMapper().readTree(thirdDict);
+            JsonNode operationDict = dict.get("GET/stateful/filter/{id}");
+
+            // Assert all elements saved in stateful_data.json
+            assertNotNull(operationDict.get("object1.numberProp1"));
+            assertEquals(4, operationDict.get("object1.numberProp1").size());
+            assertTrue(operationDict.get("object1.numberProp1").get(0).isNumber());
+            assertEquals(1, operationDict.get("object1.numberProp1").get(0).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(1).isNumber());
+            assertEquals(2, operationDict.get("object1.numberProp1").get(1).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(2).isNumber());
+            assertEquals(3, operationDict.get("object1.numberProp1").get(2).intValue());
+            assertTrue(operationDict.get("object1.numberProp1").get(3).isTextual());
+            assertEquals("3", operationDict.get("object1.numberProp1").get(3).textValue());
+            assertNotNull(operationDict.get("object1.numberProp2"));
+            assertEquals(2, operationDict.get("object1.numberProp2").size());
+            assertTrue(operationDict.get("object1.numberProp2").get(0).isNull());
+            assertEquals(null, operationDict.get("object1.numberProp2").get(0).textValue());
+            assertTrue(operationDict.get("object1.numberProp2").get(1).isNumber());
+            assertEquals(5, operationDict.get("object1.numberProp2").get(1).intValue());
+            assertNotNull(operationDict.get("stringProp1"));
+            assertEquals(2, operationDict.get("stringProp1").size());
+            assertTrue(operationDict.get("stringProp1").get(0).isTextual());
+            assertEquals("string1", operationDict.get("stringProp1").get(0).textValue());
+            assertTrue(operationDict.get("stringProp1").get(1).isNull());
+            assertEquals(null, operationDict.get("stringProp1").get(1).textValue());
+            assertNotNull(operationDict.get("booleanProp"));
+            assertEquals(2, operationDict.get("booleanProp").size());
+            assertTrue(operationDict.get("booleanProp").get(0).isBoolean());
+            assertEquals(false, operationDict.get("booleanProp").get(0).booleanValue());
+            assertTrue(operationDict.get("booleanProp").get(1).isTextual());
+            assertEquals("true", operationDict.get("booleanProp").get(1).textValue());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            fail(ex.getMessage());
+        }
 
         FileManager.deleteFile(dirPath + "/stateful_data.json");
     }
