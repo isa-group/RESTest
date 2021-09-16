d44b5d941668e62f2376c6c958e29c4a5dc07fa2
Alberto Martín López <alberto.martin95@gmail.com>
2021-09-16 02:59:02 -0700

    Improved launch and stop scripts

--- scripts/launch_benchmark.sh
+++ scripts/launch_benchmark.sh
@@ -6,8 +6,27 @@
 # ./launch_benchmark.sh deletepreviousresults=false
 # ./launch_benchmark.sh deletepreviousresults false logToFile=true
 
+n_props=0
+n_java=0
+n_java_command="ps a | grep java | grep -v -w "$(basename "$0")" | grep -v -w grep | wc -l"
+
 for propsFile in `find src/test/resources/taas_eval -name props.properties`
 do
 	java -jar restest.jar $propsFile $@ &>/dev/null &
+	((n_props+=1))
 	sleep 1m
+  n_current_java="$(eval $n_java_command)"
+  if [[ "$n_props" != "$n_current_java" ]]; then
+    echo "ERROR: Instance failed: $propsFile"
+  else
+    echo "Instance correctly deployed: $propsFile"
+  fi
 done
+
+n_java_final="$(eval $n_java_command)"
+
+if [[ "$n_props" != "$n_java_final" ]]; then
+  echo "WARNING: The number of properties files and Java processes don't match"
+else
+  echo "SUCCESS: All instances running"
+fi
