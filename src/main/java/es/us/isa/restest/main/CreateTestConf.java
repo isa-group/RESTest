ad5e8a5bb812cd6ee16786668e6a3ef1bcdd57ce
Giuliano <agiulianomirabella@gmail.com>
2021-10-21 16:55:17 +0200

    Ohsome tooo slow, YouTube auth not working

--- src/main/java/es/us/isa/restest/main/CreateTestConf.java
+++ src/main/java/es/us/isa/restest/main/CreateTestConf.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class CreateTestConf {
 
     private static final Logger log = LogManager.getLogger(CreateTestConf.class);
-    private static String openApiSpecPath = "src/test/resources/GitHub/openapi.yaml";           // OAS file path
+    private static String openApiSpecPath = "src/test/resources/Folder/openapi.yaml";           // OAS file path
     private static String confPath;																// Test configuration path
 
     
