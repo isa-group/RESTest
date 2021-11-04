d353d3fc2895e18e7643d74f0c7cc51953ddd66a
Alberto Martín López <alberto.martin95@gmail.com>
2021-11-04 13:39:27 -0700

    Improvements in coverage measurements features and main()
    (cherry picked from commit e3c4604dbad6250e9fc892ba8879afc0dd363b3b)

--- src/main/java/es/us/isa/restest/coverage/CoverageComputation.java
+++ src/main/java/es/us/isa/restest/coverage/CoverageComputation.java
@@ -1,13 +1,18 @@
 package es.us.isa.restest.coverage;
 
 import es.us.isa.restest.specification.OpenAPISpecification;
+import es.us.isa.restest.testcases.TestCase;
+import es.us.isa.restest.testcases.TestResult;
 import es.us.isa.restest.util.PropertyManager;
 import es.us.isa.restest.util.TestManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.javatuples.Pair;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,23 +28,37 @@ import static es.us.isa.restest.util.FileManager.deleteFile;
  */
 public class CoverageComputation {
 
+    private static Logger logger = LogManager.getLogger(CoverageComputation.class.getName());
+
     public static void main(String[] args) {
-        String specPath = "src/test/resources/DHL/swagger.yaml";
+        String specPath = "src/test/resources/restest-test-resources/coverage-data/swagger.yaml";
         OpenAPISpecification spec;
-        String testDirPath = "target/test-data/DHL_semantic_address";
+        String testDirPath = "src/test/resources/restest-test-resources/coverage-data";
         File testDir;
+        int batchSize = Integer.MAX_VALUE;
         CoverageMeter coverageMeter;
         List<Pair<String, String>> testCasesResultsFiles = new ArrayList<>(); // left = testCasesFile; right = testResultsFile
 
         // Validation of arguments
-        if (args.length != 0 && args.length != 2) {
-            throw new IllegalArgumentException("You must provide two arguments: 1) path to OAS, and 2) path to " +
-                    "folder containing test cases and test results in CSV format.");
+        if (args.length != 0 && args.length != 2 && args.length != 3) {
+            throw new IllegalArgumentException("You must provide two or three arguments: 1) path to OAS,2) path to " +
+                    "folder containing test cases and test results in CSV format, and 3) batch size for computing " +
+                    "coverage (optional, but improve performance for very large test suites, e.g., >100K test cases.");
         }
 
         if (args.length == 2) {
             specPath = args[0];
             testDirPath = args[1];
+        }
+
+        if (args.length == 3) {
+            try {
+                batchSize = Integer.parseInt(args[2]);
+                if (batchSize <= 0)
+                    throw new Exception();
+            } catch (Exception e) {
+                throw new IllegalArgumentException("The batch size must be an integer greater than 0.");
+            }
         }
 
         try {
@@ -64,6 +83,8 @@ public class CoverageComputation {
         }
 
         // Load test cases and test results files
+        logger.info("Reading CSV files (test cases and test results)");
+
         List<String> dirFiles = Arrays.asList(testDir.list());
         List<String> testCasesFiles = dirFiles.stream().filter(f -> f.startsWith("test-cases")).collect(Collectors.toList());
 
@@ -74,9 +95,15 @@ public class CoverageComputation {
         }
 
         // Add test cases and test results to coverageMeter progressively
+        int i = 1;
+        int total = testCasesResultsFiles.size();
+        logger.info("Computing coverage");
         for (Pair<String, String> testCasesResultsFile: testCasesResultsFiles) {
-            coverageMeter.addTestSuite(TestManager.getTestCases(testCasesResultsFile.getValue0()));
-            coverageMeter.addTestResults(TestManager.getTestResults(testCasesResultsFile.getValue1()));
+            Collection<TestCase> testSuite = TestManager.getTestCases(testCasesResultsFile.getValue0());
+            Collection<TestResult> testResults = TestManager.getTestResults(testCasesResultsFile.getValue1());
+            coverageMeter.addTestSuite(testSuite);
+            coverageMeter.addTestResults(testResults, testSuite);
+            logger.info("Progress: {}/{}", i++, total);
         }
 
         // Export coverage to CSV (both a priori and a posteriori)
@@ -86,8 +113,11 @@ public class CoverageComputation {
         deleteFile(aPrioriCoveragePath);
         deleteFile(aPosterioriCoveragePath);
 
+        logger.info("Generating coverage a priori");
         exportCoverageReport(coverageMeter, aPrioriCoveragePath);
-        exportCoverageReport(coverageMeter.getAPosteriorCoverageMeter(), aPosterioriCoveragePath);
+        logger.info("Generating coverage a posteriori");
+        exportCoverageReport(coverageMeter.getAPosteriorCoverageMeter(5005), aPosterioriCoveragePath);
+        logger.info("Coverage files generated in path {}", testDirPath);
     }
 
     private static void exportCoverageReport(CoverageMeter coverageMeter, String path) {
