e3c4604dbad6250e9fc892ba8879afc0dd363b3b
Alberto Martín López <alberto.martin95@gmail.com>
2021-11-04 13:39:27 -0700

    Improvements in coverage measurements features and main()

--- src/test/java/es/us/isa/restest/e2e/CoverageComputationTest.java
+++ src/test/java/es/us/isa/restest/e2e/CoverageComputationTest.java
@@ -6,7 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
+import static es.us.isa.restest.util.CSVManager.readCSV;
 import static es.us.isa.restest.util.FileManager.checkIfExists;
 import static es.us.isa.restest.util.FileManager.deleteFile;
 import static org.junit.Assert.*;
@@ -31,8 +33,31 @@ public class CoverageComputationTest {
             CoverageComputation.main(args);
             fail("This test should throw an exception.");
         } catch (IllegalArgumentException e) {
-            assertEquals ("You must provide two arguments: 1) path to OAS, and 2) path to " +
-                    "folder containing test cases and test results in CSV format.", e.getMessage());
+            assertEquals ("You must provide two or three arguments: 1) path to OAS,2) path to " +
+                    "folder containing test cases and test results in CSV format, and 3) batch size for computing " +
+                    "coverage (optional, but improve performance for very large test suites, e.g., >100K test cases.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void wrongThirdArgumentTest() {
+        try {
+            String[] args = {"src/test/resources/restest-test-resources/coverage-data/swagger.yaml", "src/test/resources/restest-test-resources/coverage-data", "noInteger"};
+            CoverageComputation.main(args);
+            fail("This test should throw an exception.");
+        } catch (IllegalArgumentException e) {
+            assertEquals ("The batch size must be an integer greater than 0.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void wrongThirdArgument2Test() {
+        try {
+            String[] args = {"src/test/resources/restest-test-resources/coverage-data/swagger.yaml", "src/test/resources/restest-test-resources/coverage-data", "-5"};
+            CoverageComputation.main(args);
+            fail("This test should throw an exception.");
+        } catch (IllegalArgumentException e) {
+            assertEquals ("The batch size must be an integer greater than 0.", e.getMessage());
         }
     }
 
@@ -68,6 +93,14 @@ public class CoverageComputationTest {
 
         assertTrue(checkIfExists(aPrioriCoveragePath));
         assertTrue(checkIfExists(aPosterioriCoveragePath));
+
+        List<List<String>> csvRowsAPriori = readCSV(aPrioriCoveragePath);
+        int indexAPriori = csvRowsAPriori.get(0).indexOf("STATUS_CODE//find-by-address->GET");
+        List<List<String>> csvRowsAPosteriori = readCSV(aPosterioriCoveragePath);
+        int indexAPosteriori = csvRowsAPosteriori.get(0).indexOf("STATUS_CODE//find-by-address->GET");
+
+        assertEquals("50.0", csvRowsAPriori.get(1).get(indexAPriori));
+        assertEquals("50.0", csvRowsAPosteriori.get(1).get(indexAPosteriori));
 
         deleteFile(aPrioriCoveragePath);
         deleteFile(aPosterioriCoveragePath);
