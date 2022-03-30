5055fcebbbd767224c8671bba9d3f38dacafeea6
Giuliano <agiulianomirabella@gmail.com>
2022-03-30 10:02:39 +0200

    Closing the branch

--- src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
+++ src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
@@ -34,7 +34,7 @@ import static es.us.isa.restest.util.Timer.TestStep.ALL;
 public class TestGenerationAndExecution {
 
 	// Properties file with configuration settings
-	private static String propertiesFilePath = "src/test/resources/YouTube_Videos/props.properties";
+	private static String propertiesFilePath = "src/test/resources/YouTube_CommentsAndThreads/props.properties";
 
 	private static List<String> argsList;								// List containing args
 
