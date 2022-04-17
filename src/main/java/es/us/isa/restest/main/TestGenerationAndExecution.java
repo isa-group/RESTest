0471d5a961ad4323f77dec871f0cb6482bcc231e
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2022-04-17 20:09:49 +0100

    Added OMDb and GitHub configuration files

--- src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
+++ src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
@@ -38,7 +38,7 @@ import static es.us.isa.restest.util.Timer.TestStep.ALL;
 public class TestGenerationAndExecution {
 
 	// Properties file with configuration settings
-	private static String propertiesFilePath = "src/test/resources/acmeExplorer/acmeExplorer.properties";
+	private static String propertiesFilePath = "---";
 
 	private static List<String> argsList;								// List containing args
 	
