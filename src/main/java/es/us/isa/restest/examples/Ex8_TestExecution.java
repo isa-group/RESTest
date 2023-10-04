2b450cbbd60c84e13a874185a2c5e6c1c84e6467
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2023-10-04 16:54:18 +0200

    Complete code examples 5 and 8 of the "examples" package.

--- src/main/java/es/us/isa/restest/examples/Ex8_TestExecution.java
+++ src/main/java/es/us/isa/restest/examples/Ex8_TestExecution.java
@@ -1,24 +1,34 @@
 package es.us.isa.restest.examples;
 
-import es.us.isa.restest.generators.RandomTestCaseGenerator;
-import es.us.isa.restest.runners.RESTestLoader;
-import es.us.isa.restest.testcases.TestCase;
+import es.us.isa.restest.runners.RESTestExecutor;
+import es.us.isa.restest.runners.RESTestRunner;
 import es.us.isa.restest.util.RESTestException;
-import es.us.isa.restest.writers.restassured.RESTAssuredWriter;
-
-import java.util.Collection;
-
-import static es.us.isa.restest.util.FileManager.createDir;
 
 /**
- * TODO
+ * Ex8_TestExecution class serves as an entry point for executing a series of REST API test cases using RESTestRunner.
+ *
+ * - Specify the configuration options in the user properties file located at "src/main/resources/Examples/Ex8_TestExecution/user_config.properties".
+ * - The test cases are executed using RESTestRunner, which orchestrates the test workflow.
+ * - Upon completion, the number of executed test cases is printed to the console.
+ *
+ * The resources for this example are located at src/main/resources/Examples/Ex8_TestExecution.
  *
  */
 public class Ex8_TestExecution {
 
-    public static String propertyFilePath="TODO"; 		// Path to user properties file with configuration options
+    public static String targetDir = "src/main/resources/Examples/Ex8_TestExecution/test_cases";
+
+    public static String testClassName = "RestCountriesTest";
+
+    public static String packageName = "restcountries";
+
+
 
     public static void main(String[] args) throws RESTestException {
+
+        RESTestExecutor executor = new RESTestExecutor(targetDir, testClassName, packageName);
+        executor.execute();
+
 
     }
 }
