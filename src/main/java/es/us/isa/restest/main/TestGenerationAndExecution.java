c2807e720d94b3efa5a669d295571737df504bb7
= <=>
2024-09-27 17:41:24 +0200

    Auto commit

--- src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
+++ src/main/java/es/us/isa/restest/main/TestGenerationAndExecution.java
@@ -41,7 +41,7 @@ import static es.us.isa.restest.util.Timer.TestStep.ALL;
 public class TestGenerationAndExecution {
 
 	// Properties file with configuration settings
-	private static String propertiesFilePath = "src/test/resources/YouTube_Search/props.properties";
+	private static String propertiesFilePath = "src/test/resources/GitHub/props.properties";
 
 	private static List<String> argsList;								// List containing args
 
